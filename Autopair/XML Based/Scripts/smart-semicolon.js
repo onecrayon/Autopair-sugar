d5be70529ec972eae1eb7e2f00f0774fbede7bb6
Ian Beck <ian@onecrayon.com>
2015-04-16 14:08:54 -0700

    Modified smart semicolon logic to ensure semicolons only get inserted at the end of the line (and bypass additional closing braces)

--- Autopair/XML Based/Scripts/smart-semicolon.js
+++ Autopair/XML Based/Scripts/smart-semicolon.js
@@ -21,18 +21,30 @@ action.canPerformWithContext = function(context, outError) {
 	if (sel.location + sel.length < context.string.length) {
 		nextChar = context.substringWithRange(new Range(sel.location + sel.length, 1));
 	}
-	return (prevChar === '[' && nextChar === ']') || (prevChar === '{' && nextChar === '}') || (prevChar === '(' && nextChar === ')');
+	var proceed = (prevChar === '[' && nextChar === ']') || (prevChar === '{' && nextChar === '}') || (prevChar === '(' && nextChar === ')');
+	if (proceed) {
+		// Ensure that we're at the end of the line
+		var lineRange = context.lineStorage.lineRangeForIndex(sel.location);
+		nextChar = context.substringWithRange(new Range(sel.location, lineRange.location + lineRange.length - sel.location));
+		proceed = /^[)}\]]+(?:[\r\n]+)?$/.test(nextChar);
+	}	
+	return proceed;
 };
 
 action.performWithContext = function(context, outError) {
 	var sel = context.selectedRanges[0],
-		nextChar = context.substringWithRange(new Range(sel.location + sel.length, 1));
-	var snippet;
-	if (action.setup.forceLinebreak === 'yes') {
-		snippet = new CETextSnippet('\n\t$0\n' + nextChar + ';')
-	} else {
-		snippet = new CETextSnippet(nextChar + ';$0');
+		lineRange = context.lineStorage.lineRangeForIndex(sel.location),
+		charsLen = lineRange.location + lineRange.length - sel.location,
+		nextChars = context.substringWithRange(new Range(sel.location, charsLen)),
+		snippet;
+	if (/[\r\n]+$/.test(nextChars)) {
+		nextChars = nextChars.replace(/^([^\r\n]+)[\r\n]+$/, '$1');
 	}
-	context.selectedRanges = [new Range(sel.location, 1)];
+	if (action.setup.forceLinebreak === 'yes') {
+		snippet = new CETextSnippet('\n\t$0\n' + nextChars + ';')
+	} else {
+		snippet = new CETextSnippet(nextChars + ';$0');
+	}
+	context.selectedRanges = [new Range(sel.location, nextChars.length)];
 	return context.insertTextSnippet(snippet, CETextOptionNormalizeIndentationLevel | CETextOptionNormalizeLineEndingCharacters | CETextOptionNormalizeIndentationCharacters);
 };
